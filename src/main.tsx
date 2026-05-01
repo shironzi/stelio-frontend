ec377a8444cb2d15737d84fcbc5fb0fdcdbeaa61
shironzi <157195223+shironzi@users.noreply.github.com>
2026-05-01 21:49:24 +0800

    refactor: remove StrictMode wrapper from root rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { PropertyProvider } from "./context/PropertyContext";
 import { UserProvider } from "./context/UserContext";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <UserProvider>
-      <PropertyProvider>
-        <App />
-      </PropertyProvider>
-    </UserProvider>
-  </StrictMode>,
+  <UserProvider>
+    <PropertyProvider>
+      <App />
+    </PropertyProvider>
+  </UserProvider>,
 );
